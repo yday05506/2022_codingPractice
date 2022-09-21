597f5c0ef76658b0f0e83c73a6b1bde5b520f781
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2022-09-21 21:25:34 +0900

    하샤드 수

---
+++ HarshadNum.java
@@ -0,0 +1,17 @@
+public class HarshadNum {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int num = x;
+
+        while(num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        if(x % sum == 0) answer = true;
+        else answer = false;
+
+        return answer;
+    }
+}
