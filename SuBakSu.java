bc40d0910c3f476d1a36517a8c8c157fafeb178f
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2022-09-26 21:13:11 +0900

    수박수박수박수

---
+++ SuBakSu.java
@@ -0,0 +1,11 @@
+public class SuBakSu {
+    public String solution(int n) {
+        String answer = "";
+
+        for(int i = 0; i < n; i++) {
+            answer += i % 2 == 0 ? "수" : "박";
+        }
+
+        return answer;
+    }
+}
