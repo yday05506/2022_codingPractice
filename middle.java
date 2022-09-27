e9c9bee793b31312d5edac87fb919349426c1dc5
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2022-09-27 23:42:32 +0900

    가운데 문자 반환

---
+++ middle.java
@@ -0,0 +1,12 @@
+public class middle {
+    public String solution(String s) {
+        String answer = "";
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.length() % 2 == 1) answer = s.substring(s.length() / 2, s.length() / 2 + 1);
+            else answer = s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+        }
+
+        return answer;
+    }
+}
