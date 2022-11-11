5bd01e0d8a147fe6ea1240dedb7ec768ed7d237b
yday <yangdayeon2@gmail.com>
2022-11-11 12:44:26 +0900

    비밀번호 검사하기

---
+++ correctPassword.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool solution(char* password) {
+	bool answer;
+
+	int capital_count = 0, small_count = 0, digit_count = 0;
+	for (int i = 0; i < strlen(password); i++) {
+		if (password[i] >= 'A' && password[i] <= 'Z')
+			capital_count++;
+		else if (password[i] >= 'a' && password[i] <= 'z')
+			small_count++;
+		else if (password[i] >= '0' && password[i] <= '9')
+			digit_count++;
+	}
+
+	if (capital_count >= 1 && small_count >= 2 && digit_count >= 2)
+		answer = true;
+	else
+		answer = false;
+
+	return answer;
+}
+
+int main() {
+	char* password1 = "helloworld";
+	bool ret1 = solution(password1);
+
+	printf("solution 함수의 반환 값은 %s 입니다.\n", ret1 ? "true" : "false");
+
+	char* password2 = "Hello123";
+	bool ret2 = solution(password2);
+
+	printf("solution 함수의 반환 값은 %s 입니다.\n", ret2 ? "true" : "false");
+}
