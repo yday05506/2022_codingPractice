3cc9234c75a1cb501248fd88f64ff9b601d433e2
Dayeon Yang <85325266+yday05506@users.noreply.github.com>
2023-01-06 11:49:16 +0900

    cospro 6일차 연습

---
+++ 6차 2급 4_initial_code.c
@@ -0,0 +1,50 @@
+// 다음과 같이 include를 사용할 수 있습니다.
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(char* cards[][2], int cards_len) {
+     // 여기에 코드를 작성해주세요.
+    int answer = 0;
+    int count[3] ={0, };
+    
+    for(int i = 0; i < cards_len; i++) {
+    	if(strcmp(cards[i][0], "blue")==0) count[0]++;
+    	else if(strcmp(cards[i][0], "red")==0) count[1]++;
+    	else if(strcmp(cards[i][0], "black")==0) count[2]++;
+	}
+    
+    for(int i = 0; i < cards_len; i++) {
+    	answer += atoi(cards[i][1]);
+	}
+	
+	for(int i = 0; i < 3; i++) {
+		if(count[i] == 3) {
+			answer *= 3;
+			break;
+		}
+		else if(count[i] == 2) {
+			answer *= 2;
+			break;
+		}
+	}
+    
+    return answer;
+}
+
+// 아래는 테스트케이스 출력을 해보기 위한 main 함수입니다.
+int main() {
+    char* cards1[][2] = {{"blue", "2"}, {"red", "5"}, {"black", "3"}};
+    int cards_len1 = 3;
+    int ret1 = solution(cards1, cards_len1);
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret1);
+
+    char* cards2[][2] = {{"blue", "2"}, {"blue", "5"}, {"black", "3"}};
+    int cards_len2 = 3;
+    int ret2 = solution(cards2, cards_len2);
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    printf("solution 함수의 반환 값은 %d 입니다.\n", ret2);
+}
